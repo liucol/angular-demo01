737b8ae3f694c2ee08c161935443623fac84c424
yangliu <pearl-iooi@foxmail.com>
2018-10-08 14:38:01 +0800

    first

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header', //使用这个组件的名称
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title = "这是一个header组件";
+  msg: any;
+  constructor() { } //构造函数  一初始化就会调用这个方法
+
+  ngOnInit() { //生命周期函数
+    this.msg = "这是一个angular的数据";
+  }
+
+}
